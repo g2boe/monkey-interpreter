6a772c9fa7450b5a3a5908947beca23d43e21d18
Grant Boehringer <g2boehringer@gmail.com>
2025-09-24 22:58:12 -0500

    Add evaluator

---
+++ object/object.go
@@ -0,0 +1,90 @@
+package object
+
+import (
+	"bytes"
+	"fmt"
+	"monkey/ast"
+	"strings"
+)
+
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ = "ERROR"
+	FUNCTION_OBJ = "FUNCTION"
+)
+
+type ObjectType string
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+
+
+type Integer struct {
+	Value int64
+}
+
+type Boolean struct {
+	Value bool
+}
+
+type ReturnValue struct {
+	Value Object
+}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+
+type Null struct{}
+
+type Error struct {
+	Message string
+}
+
+// Integer functions
+func (integer *Integer) Inspect() string { return fmt.Sprintf("%d", integer.Value) }
+func (integer *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+// Bool functions
+func (boolean *Boolean) Inspect() string { return fmt.Sprintf("%t", boolean.Value) }
+func (boolean *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+// Return functions
+func (retVal *ReturnValue) Inspect() string { return retVal.Inspect() }
+func (retVal *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+
+// Null functions
+func (null *Null) Inspect() string { return "null" }
+func (null *Null) Type() ObjectType { return NULL_OBJ }
+
+// Error functions
+func (err *Error) Inspect() string { return "ERROR: " + err.Message }
+func (err *Error) Type() ObjectType { return ERROR_OBJ }
+
+// Function functions
+func (fun *Function) Inspect() string {
+	var out bytes.Buffer
+	params := []string{}
+
+	for _, p := range fun.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(fun.Body.String())
+	out.WriteString("\n}")
+
+	return out.String()
+}
+func (fun *Function) Type() ObjectType { return FUNCTION_OBJ }
